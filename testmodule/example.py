12972a374b062c2b7583e1154a12239aeb8dfe19
Hollis Akins <hollis@Holliss-MacBook-Pro.local>
2024-11-28 14:16:15 -0500

    test

--- testmodule/example.py
+++ testmodule/example.py
@@ -1,8 +1,27 @@
+"""
+Example module for documentation. 
+
+Here I am writing some random text, which will be included in the documentation. 
+Most of it is AI-generated, so it doesn't make much sense.
+I'm mostly just rambling on about nothing in particular, trying to fill up space.
+
+Example usage:
+
+
+.. highlight:: python
+
+::
+
+    from testmodule import ExampleClass
+    e = ExampleClass('test')
+"""
 
 
 class ExampleClass:
     '''Docstring for example class'''
     def __init__(self, name):
+
+        '''Name of the object'''
         self.name = name
 
     def method(self, prop):
