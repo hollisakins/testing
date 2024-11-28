6a645af33fa3e9ab3c1e1520f908e431833e4140
Hollis Akins <hollis@Holliss-MacBook-Pro.local>
2024-11-28 13:04:40 -0500

    test

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,32 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",  # core library for html generation from docstrings
     "sphinx.ext.autosummary",  # create neat summary tables
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "IPython.sphinxext.ipython_console_highlighting",
+    # "sphinx_gallery.gen_gallery",
+    "sphinx_toolbox.collapse",
+    "sphinx_copybutton",  # Add a copy button to code blocks
 ]
+
+# sphinx_gallery_conf = {
+#     "examples_dirs": "../../examples",  # path to your example scripts
+#     # Path to where to save gallery generated output
+#     "gallery_dirs": "auto_examples",
+#     "nested_sections": True,
+#     # Directory where function/class granular galleries are stored
+#     "backreferences_dir": "_autosummary/backreferences",
+#     # Modules for which function/class level galleries are created
+#     "doc_module": ("synthesizer"),
+#     "abort_on_example_error": True,
+#     # "show_memory": True,
+#     # "recommender": {
+#     #     "enable": True,
+#     #     "n_examples": 5,
+#     #     "min_df": 3,
+#     #     "max_df": 0.9,
+#     # },
+# }
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
